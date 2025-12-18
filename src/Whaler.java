1e53c051e249a205df9c0be76f04aba4cb0e2129
renhirose <ren_hirose28@milton.edu>
2025-12-18 09:28:43 -0500

    Initial commit

---
+++ src/Whaler.java
@@ -0,0 +1,37 @@
+public class Whaler {
+    public String name;                //holds the name of the hero
+    public int xpos;                //the x position
+    public int ypos;                //the y position
+    public int dx;                    //the speed of the hero in the x direction
+    public int dy;                    //the speed of the hero in the y direction
+    public int width;
+    public int height;
+    public boolean isAlive;            //a boolean to denote if the hero is alive or dead.
+
+
+    // METHOD DEFINITION SECTION
+
+    // Constructor Definition
+    // A constructor builds the object when called and sets variable values.
+
+
+    //This is a SECOND constructor that takes 3 parameters.  This allows us to specify the hero's name and position when we build it.
+    // if you put in a String, an int and an int the program will use this constructor instead of the one above.
+    public Whaler(int pXpos, int pYpos) {
+        xpos = pXpos;
+        ypos = pYpos;
+        dx =1;
+        dy =6;
+        width = 150;
+        height = 100;
+        isAlive = true;
+
+    } // constructor
+
+    //The move method.  Everytime this is run (or "called") the hero's x position and y position change by dx and dy
+    public void move() {
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+
+    }
+}
